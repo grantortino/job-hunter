fecca1a6431c497a520dca120897f8691685fc50
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-09 17:31:41 +0100

    structuring job tracker section

---
+++ src/components/StartScreen.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import Screen from './Screen';
+
+const StartScreen = () => {
+    return (
+        <Screen>
+        <div>
+          <h1>JOB HUNTER</h1>
+          <div className="sections-container">
+            <div className="job-hunter-section">
+
+            </div>
+            <div className="job-journal-section">
+
+            </div>
+            <div className="job-interview-section">
+              
+            </div>
+          </div>
+        </div>
+      </Screen>
+    );
+};
+
+export default StartScreen;
---
+++ src/components/TrackerScreen.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const TrackerScreen = () => {
+    return (
+        <div>
+            <h3>JOB TRACKER</h3>
+            <div className="tracker-sections-container">
+                
+            </div>
+        </div>
+    );
+};
+
+export default TrackerScreen;
