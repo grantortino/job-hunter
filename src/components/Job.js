3d798091e9ddd37479292622b6e0068946d6a496
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-20 15:59:30 +0100

    styling job cards

--- src/components/Job.js
+++ src/components/Job.js
@@ -3,16 +3,35 @@ import React, { useState } from 'react';
 import jobstore from '../stores/JobStore';
 // assets
 import jobPaw from '../assets/job-hunter-characters/job-paw.png';
+import jobPawAccepted from '../assets/job-hunter-characters/job-hunter-svg/tracked-job-paw-accepted.svg';
+import jobPawRejected from '../assets/job-hunter-characters/job-hunter-svg/tracked-job-paw-rejected.svg';
+import jobPawPending from '../assets/job-hunter-characters/job-hunter-svg/tracked-job-paw-pending.svg';
 
-const Job = ({ job }) => {
+
+const Job = ({ job, status }) => {
 
     // when I modify the state, consequentially the component re-renders and displays the filtered jobs - it is a dirty hack
     const [quickHack, setQuickHack] = useState(false);
+    const [image, setImage] = useState();
 
-    console.log(job.job)
+    // const setImageHandler = () => {
+    //     if (job.status === "rejected") {
+    //         setImage(jobPawRejected);
+    //         return image;
+    //     } else if (job.status === "accepted") {
+    //         setImage(jobPawAccepted);
+    //         return image;
+    //     } else {
+    //         setImage(jobPawPending);
+    //         return image;
+    //     }
+    // };
+
+    console.log(status)
+    
     return (
-        <div>
-            <img className="job-paw-img" src={jobPaw} alt="Job Icon"/>
+        <div className="job-card">
+            <img className="job-paw-img" src={jobPawPending} alt="Job Icon"/>
             <div className="small">{job.companyName === undefined ? 'Company Name Missing' : job.companyName.toUpperCase()}</div>
             <div className="font-small">{job.jobTitle === undefined ? 'Job Title Missing' : job.jobTitle}</div>
             <a rel="noreferrer" className="font-small" href={job.jobLink} target="_blank">LINK</a>
--- src/components/TrackedJobs.js
+++ src/components/TrackedJobs.js
@@ -10,23 +10,26 @@ const TrackedJobs = () => {
 
     return (
         <Screen>
-            <div className="job-tracker-form-container">
+            <div className="tracked-jobs-container">
+
                 <h1 className="medium">TRACKED JOBS</h1>
 
-                <div className="job-card">
-                    {/* <img src={jobHunter} alt="job logo" />
-                    <h1 className="medium">{jobs.companyName}</h1> */}
-                    {jobstore.jobs.map((job) => (
-                        // <div key={job.id} >
-                        //     <img className="job-paw-img" src={jobPaw} alt="Job Icon"/>
-                        //     <div className="small">{job.companyName === undefined ? 'Company Name Missing' : job.companyName.toUpperCase()}</div>
-                        //     <div className="font-small">{job.jobTitle === undefined ? 'Job Title Missing' : job.jobTitle}</div>
-                        //     <a rel="noreferrer" className="font-small" href={job.jobLink} target="_blank">LINK</a>
-                        //     <div className="font-small">{job.date}</div>
-                        //     <div className="font-small pointer" onClick={() => {jobstore.remove(job.id); setQuickHack(!quickHack)}}>ERASE</div>
-                        // </div>
-                        <Job key={job.id} job={job} />
-                    ))}
+                <div className="job-tracker-overscroll-container">
+                    <div className="job-cards">
+                        {/* <img src={jobHunter} alt="job logo" />
+                        <h1 className="medium">{jobs.companyName}</h1> */}
+                        {jobstore.jobs.map((job) => (
+                            // <div key={job.id} >
+                            //     <img className="job-paw-img" src={jobPaw} alt="Job Icon"/>
+                            //     <div className="small">{job.companyName === undefined ? 'Company Name Missing' : job.companyName.toUpperCase()}</div>
+                            //     <div className="font-small">{job.jobTitle === undefined ? 'Job Title Missing' : job.jobTitle}</div>
+                            //     <a rel="noreferrer" className="font-small" href={job.jobLink} target="_blank">LINK</a>
+                            //     <div className="font-small">{job.date}</div>
+                            //     <div className="font-small pointer" onClick={() => {jobstore.remove(job.id); setQuickHack(!quickHack)}}>ERASE</div>
+                            // </div>
+                            <Job key={job.id} job={job} />
+                        ))}
+                    </div>
                 </div>
 
                 <Link to="/tracker">
