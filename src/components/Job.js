6cc36a652d270e157ec76a0a08434052e5f480d7
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-20 20:49:42 +0100

    styled job cards

--- src/components/Job.js
+++ src/components/Job.js
@@ -27,17 +27,24 @@ const Job = ({ job, status }) => {
     //     }
     // };
 
-    console.log(status)
+    const textAlign = {textAlign: "center"};
     
+    const convertDate = (dateString) => {
+        var date = new Date(dateString);
+        return date.getDate()+"/"+(date.getMonth() + 1)+"/"+date.getFullYear();
+    };
+
     return (
         <div className="job-display-block">
             <div className="job-card">
                 <img className="job-paw-img" src={jobPawPending} alt="Job Icon"/>
-                <div className="small">{job.companyName === undefined ? 'Company Name Missing' : job.companyName.toUpperCase()}</div>
-                <div className="font-small">{job.jobTitle === undefined ? 'Job Title Missing' : job.jobTitle}</div>
-                <a rel="noreferrer" className="font-small" href={job.jobLink} target="_blank">LINK</a>
-                <div className="font-small">{job.date}</div>
-                <div className="font-small pointer" onClick={() => {jobstore.remove(job.id); console.log('click!'); setQuickHack(!quickHack)}}>ERASE</div>
+                <div className="medium underline" style={textAlign}>{job.companyName === undefined ? 'Company Name Missing' : job.companyName.toUpperCase()}</div>
+                {/* <div className="font-medium">{job.jobTitle === undefined ? 'Job Title Missing' : job.jobTitle}</div> */}
+                <div className="font-medium" style={textAlign}>Front-End Web Developer</div>
+                <a rel="noreferrer" className="font-medium" href={job.jobLink} target="_blank">LINK</a>
+                {/* <div className="font-medium">{job.date}</div> */}
+                <div className="font-medium">{convertDate(job.date)}</div>
+                <div className="font-medium pointer" onClick={() => {jobstore.remove(job.id); console.log('click!'); setQuickHack(!quickHack)}}>ERASE</div>
             </div>
         </div>
     )
