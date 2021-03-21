1015c2084d760e5557523e04e1ecc8a8b9423128
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-03-21 10:47:41 +0100

    structured first modal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -3,7 +3,18 @@ import React from 'react';
 const Modal = () => {
     return (
         <div className="modal-container">
-            
+            <h1 className="huge">ARE YOU SURE YOU WANT TO SAVE:</h1>
+            <h3 className="font-medium">Company Name</h3>
+            <div className="modal-buttons-container">
+                <div className="modal-button-card">
+                    <button className="buttons">YES</button>
+                    <p className="font-small">And straight to the tracked jobs hub.</p>
+                </div>
+                <div className="modal-button-card">
+                    <button className="buttons">NO</button>
+                    <p className="font-small">And back to the hunt.</p>
+                </div>
+            </div>
         </div>
     );
 };
