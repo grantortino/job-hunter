99f092d532cd65bd9187a26490ee68cacfe49c84
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-08-04 21:48:08 +0200

    added year filter function and reduce

--- src/stores/JobStore.js
+++ src/stores/JobStore.js
@@ -1,97 +1,156 @@
-import uuid from 'react-uuid';
-import DateFormat from '../utils/DateFormat';
+import uuid from "react-uuid";
+import DateFormat from "../utils/DateFormat";
 
 const jobstore = {
+  jobs: [],
 
-    jobs: [],
+  entries: [],
 
-    entries: [],
+  filteredJobs: [],
 
-    filteredJobs: [],
+  addJob: function (
+    companyName,
+    jobTitle,
+    jobLink,
+    date,
+    relevanceDegree,
+    status
+  ) {
+    // "this" refers to the scope
+    this.jobs.push({
+      companyName,
+      jobTitle,
+      jobLink,
+      date,
+      relevanceDegree,
+      status,
+      id: uuid(),
+    });
+  },
 
-    addJob: function(companyName, jobTitle, jobLink, date, relevanceDegree, status) {
-        // "this" refers to the scope
-        this.jobs.push({companyName, jobTitle, jobLink, date, relevanceDegree, status, id: uuid()});
-    },
+  addEntry: function (entryTitle, entryContent, checkbox) {
+    this.entries.push({
+      entryTitle,
+      entryContent,
+      id: uuid(),
+      time: new Date(),
+      checkbox,
+    });
+  },
 
-    addEntry: function(entryTitle, entryContent, checkbox) {
-        this.entries.push({ entryTitle, entryContent, id:uuid(), time: new Date(), checkbox });
-    },
+  save: function () {
+    localStorage.setItem("savedJob", JSON.stringify(this.jobs));
+    localStorage.setItem("savedEntries", JSON.stringify(this.entries));
+  },
 
-    save: function() {
-        localStorage.setItem('savedJob', JSON.stringify(this.jobs));
-        localStorage.setItem('savedEntries', JSON.stringify(this.entries));
-    },
+  load: function () {
+    // gets the element from storage saved under "savedCompanyName"
+    const savedJob = localStorage.getItem("savedJob");
+    const savedEntries = localStorage.getItem("savedEntries");
+    // this.jobs = savedJob || [];
 
-    load: function() {
-        // gets the element from storage saved under "savedCompanyName"
-        const savedJob = localStorage.getItem('savedJob');
-        const savedEntries = localStorage.getItem('savedEntries');
-        // this.jobs = savedJob || [];
+    if (savedJob === null) {
+      this.jobs = [];
+    } else {
+      this.jobs = JSON.parse(savedJob);
+    }
 
-        if (savedJob === null) {
-            this.jobs = [];
-        } else {
-            this.jobs = JSON.parse(savedJob);
-        }
+    if (savedEntries === null) {
+      this.entries = [];
+    } else {
+      this.entries = JSON.parse(savedEntries);
+      // this.entries.map((entry) => entry.time = new Date(entry.time));
+      this.entries.forEach((entry) => (entry.time = new Date(entry.time)));
+    }
+  },
 
-        if (savedEntries === null) {
-            this.entries = [];
-        } else {
-            this.entries = JSON.parse(savedEntries);
-            // this.entries.map((entry) => entry.time = new Date(entry.time));
-            this.entries.forEach(entry => entry.time = new Date(entry.time));
-        }
+  remove: function (jobId) {
+    // I MADE IT!
+    this.jobs = this.jobs.filter((job) => job.id !== jobId);
+    localStorage.setItem("savedJob", JSON.stringify(this.jobs));
+  },
 
-    },
+  removeEntry: function (entryId) {
+    this.entries = this.entries.filter((entry) => entry.id !== entryId);
+    localStorage.setItem("savedEntries", JSON.stringify(this.entries));
+  },
 
-    remove: function(jobId) {
-        // I MADE IT!
-        this.jobs = this.jobs.filter((job) => job.id !== jobId);
-        localStorage.setItem('savedJob', JSON.stringify(this.jobs));
-    },
+  searchJob: function (searchValue) {
+    if (searchValue === "") {
+      return this.jobs;
+    } else {
+      return (this.filteredJobs = this.jobs.filter((job) =>
+        job.companyName.toLowerCase().includes(searchValue.toLowerCase())
+      ));
+    }
+  },
 
-    removeEntry: function (entryId) {
-        this.entries = this.entries.filter((entry) => entry.id !== entryId);
-        localStorage.setItem('savedEntries', JSON.stringify(this.entries));
-    },
+  findJob: function (id) {
+    return this.jobs.filter((job) => job.id === id)[0];
+    // const jobElement = this.jobs.filter((job) => job.id === id);
+    // return jobElement[0];
+  },
 
-    searchJob: function(searchValue) {
+  groupedEntriesByDay: function (month, year) {
+    // const groupedEntries = {};
 
-        if (searchValue === "") {
-            return this.jobs;
-        } else {
-            return this.filteredJobs = this.jobs.filter((job) => job.companyName.toLowerCase().includes(searchValue.toLowerCase()));
-        }
+    const entriesForThisMonthAndYear = this.entries
+      .filter((entry) => entry.time.getMonth() + 1 == month)
+      .filter((entry) => entry.time.getFullYear() === year);
+    // const entries = [{time: "2021-07-01", name: "coding with tom"}, {time: "2021-07-01", name: "coding with nicolo"}, {time: "2021-07-02", name: "drinking water"}]
+    // group = {}
+    // 1. interation reduce; entry = {time: "2021-07-01", name: "coding with tom"}
+    //    keyForEntry = "1 July"
+    //    group["1 July"] exists?
+    //       not = group["1 July"] = []
+    //
+    //    group["1 July"].push(entry)
+    //
+    // group = {"1 July": [{time: "2021-07-01", name: "coding with tom"}]}
+    //
+    // 2. interation reduce; entry = {time: "2021-07-01", name: "coding with nicolo"}
+    //    keyForEntry = "1 July"
+    //    group["1 July"] already exists
+    //    group["1 July"].push(entry)
+    //
+    // group = {"1 July": [{time: "2021-07-01", name: "coding with tom"}, {time: "2021-07-01", name: "coding with nicolo"}]}
+    //
+    // 3. interation reduce; entry = {time: "2021-07-02", name: "driking water"}
+    //    keyForEntry = "2 July"
+    //    group["2 July"] doesn't exist
+    //        group["2 July"] = []
+    //    group["2 July"].push(entry)
+    //
+    // group = {"1 July": [{time: "2021-07-01", name: "coding with tom"}, {time: "2021-07-01", name: "coding with nicolo"}],
+    //          "2 July": [{time: "2021-07-02", name: "drinking water"}]}
+    // const entries = [{time: "2021-07-01", name: "coding with tom"}, {time: "2021-07-01", name: "coding with nicolo"}, , {time: "2021-07-02"}, {time: "2021-06-01"}]
+    // return this.entries
+    // .filter((entry) => entry.time.getMonth() + 1 == month)
+    // .filter((entry) => entry.time.getFullYear() === year)
+    // .reduce((group, entry) => {
+    //     const keyForEntry = DateFormat.displayNameOfDay(entry.time);
 
-    },
+    //     if (group[keyForEntry] === undefined) {
+    //       group[keyForEntry] = [];
+    //     }
 
-    findJob: function(id) {
-        return (this.jobs.filter((job) => job.id === id))[0];
-        // const jobElement = this.jobs.filter((job) => job.id === id);
-        // return jobElement[0];
-    },
+    //     group[keyForEntry].push(entry);
 
-    groupedEntriesByDay: function(month) {
+    //     return group;
+    // }, {})
 
-        const groupedEntries = {};
+    // copio tutte le entrate che appartengono allo stesso giorno.
 
-        const entriesForThisMonth = this.entries.filter((entry) => entry.time.getMonth() + 1 == month);
+    return entriesForThisMonthAndYear.reduce((group, entry) => {
+      const keyForEntry = DateFormat.displayNameOfDay(entry.time);
 
-        entriesForThisMonth.forEach((entry) => {
-            const keyForEntry = DateFormat.displayNameOfDay(entry.time);
-
-            if (groupedEntries[keyForEntry] === undefined) {
-                groupedEntries[keyForEntry] = [];
-            }
-
-            groupedEntries[keyForEntry].push(entry);
-        });
-
-        return groupedEntries;
-
-    },
-
+      if (group[keyForEntry] === undefined) {
+        group[keyForEntry] = [];
+      }
+      group[keyForEntry].push(entry);
+      return group;
+    }, {});
+  },
 };
 
 jobstore.load();
