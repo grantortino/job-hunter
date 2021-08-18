8e839098733d47271626202a92adc4f39984424a
Nicolo <54029524+grantortino@users.noreply.github.com>
2021-08-18 10:06:08 +0200

    push

--- src/stores/__tests__/JobStore.test.js
+++ src/stores/__tests__/JobStore.test.js
@@ -1,26 +1,10 @@
 import jobstore from '../JobStore';
 
-// const fakeJobs = [{id: '666'}, {id: '777'}];
-
-// test('Find the job', () => {
-//     jobstore.jobs = fakeJobs;
-//     expect(jobstore.findJob('666')).toEqual({id: '666'});
-// });
-
-// // test('Return "undefined" when job is not found', () => {
-// //     jobstore.jobs = fakeJobs;
-// //     expect(jobstore.findJob('000')).toEqual(undefined);
-// // });
-
-// test('jobstore.addJob stores in local storage', () => {
-//     jobstore.addJob('Bauli', 'Panettone Bäcker', 'http://pivo.ru', '2001-03-50', 'high', 'accepted');
-//     const lastJob = jobstore.jobs[jobstore.jobs.length - 1];
-//     expect(lastJob.companyName).toEqual('Bauli');
-//     expect(lastJob.jobTitle).toEqual('Panettone Bäcker');
-//     expect(lastJob.id).not.toBeNull();
-// });
-
 const fakeJobs = [{id: '666'}, {id: '999'}, {id: '1010'}];
+
+test('the data passed are not undefined', () => {
+    
+})
 
 test('remove job through id', () => {
     jobstore.jobs = fakeJobs;
@@ -59,6 +43,26 @@ const fakeEntries = [
 //             }
 //         ]
 //     });
+// });
+
+// const fakeJobs = [{id: '666'}, {id: '777'}];
+
+// test('Find the job', () => {
+//     jobstore.jobs = fakeJobs;
+//     expect(jobstore.findJob('666')).toEqual({id: '666'});
+// });
+
+// // test('Return "undefined" when job is not found', () => {
+// //     jobstore.jobs = fakeJobs;
+// //     expect(jobstore.findJob('000')).toEqual(undefined);
+// // });
+
+// test('jobstore.addJob stores in local storage', () => {
+//     jobstore.addJob('Bauli', 'Panettone Bäcker', 'http://pivo.ru', '2001-03-50', 'high', 'accepted');
+//     const lastJob = jobstore.jobs[jobstore.jobs.length - 1];
+//     expect(lastJob.companyName).toEqual('Bauli');
+//     expect(lastJob.jobTitle).toEqual('Panettone Bäcker');
+//     expect(lastJob.id).not.toBeNull();
 // });
 
 test('groupedEntriesByDay returns entries on the same day as the key', () => {
